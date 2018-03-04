2086863b8f51e9b1b4ef231486a4a93ee68b0831
Alexander Popov <rastopyakin@gmail.com>
2018-03-04 18:23:03 +0700

    Switch to Boost Testing framework

--- helloworld/test/main_test.cpp
+++ helloworld/test/main_test.cpp
@@ -1,5 +1,12 @@
 #include "hw.hpp"
 
-int main() {
-	return !(version() > 0);
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+#define BOOST_TEST_MODULE hw_tests
+
+BOOST_AUTO_TEST_CASE(version_test)
+{
+	BOOST_REQUIRE(version() > 0);
 }
