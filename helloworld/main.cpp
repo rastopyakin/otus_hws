124d587f5def9d892d32a212173dcc2b306f6e32
Alexander Popov <rastopyakin@gmail.com>
2018-03-13 15:54:00 +0700

    Add version switch (helloworld)

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -3,7 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-	std::printf("Hello, world!\n");
-	std::printf("Version %s\n", version().c_str());
+	if (argc == 1)
+		std::printf("Hello, world!\n");
+	else if (std::string{argv[1]} == "--version")
+		std::printf("Version %s\n", version().c_str());
+	else
+		std::printf("Usage: helloworld [--version]\n");
 	return 0;
 }
