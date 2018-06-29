91ce587e0246c0192bb509951f41b08d257e5776
Alexander Popov <rastopyakin@gmail.com>
2018-06-29 17:12:12 +0700

    Fill the conteiners (allocator)

--- allocator/main.cpp
+++ allocator/main.cpp
@@ -13,20 +13,31 @@ constexpr T factorial(T i) {
 
 int main(int argc, char *argv[])
 {
-    const int N = 3;
+    const int N = 10;
+    std::printf("standart allocator map\n");
     std::map<int, int> m;
     for (int i = 0; i < N; i++)
 	m.insert(std::make_pair(i, factorial(i)));
 
-    for (const auto &p : m) {
+    std::printf("reserving allocator map\n");
+    std::map<int, int, std::less<int>, reserving_allocator<std::pair<int, int>, N>> mc;
+    for (int i = 0; i < N; i++)
+	mc.insert(std::make_pair(i, factorial(i)));
+    for (const auto &p : mc) {
       std::printf("%d %d\n", p.first, p.second);
     }
 
+    std::printf("standart allocator list\n");
     tsk::list<int> l;
     for (int i = 0; i < N; i++)
 	l.push_front(i);
 
-    for (const auto &p: l)
+    std::printf("reserving allocator list\n");
+    tsk::list<int, reserving_allocator<int, N>> lc;
+    for (int i = 0; i < N; i++)
+	lc.push_front(i);
+
+    for (const auto &p: lc)
 	std::printf("%d\n", p);
 
     std::printf("the end!\n");
