bbdfd907bb4147743da017c7e21ca1a3b869a1ae
Alexander Popov <rastopyakin@gmail.com>
2018-06-29 12:54:05 +0700

    Add allocator parameter to list template (allocator)
    Minor improvement: avoid unneccesary copying via moving data

--- allocator/list.hpp
+++ allocator/list.hpp
@@ -1,17 +1,23 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+#include <memory>
+#include <utility>
 namespace tsk {
-    template <class T>
+    template <class T, class Allocator = std::allocator<T>>
     class list {
     private:
 	struct node {
-	    T data;
+	    node(T && data_, node* next_) :
+		next(next_), data(std::move(data_)) {}
 	    node *next;
+	    T data;
 	};
 
 	node *head = nullptr;
-
+	// using node_allocator = typename std::allocator_traits<Allocator>:: template rebind_alloc<node> ;
+	using node_allocator = typename Allocator:: template rebind<node>::other;
+	node_allocator a;
     public:
 	class iterator {
 	public:
@@ -35,17 +41,22 @@ namespace tsk {
 	iterator end() {
 	    return iterator{nullptr};
 	}
-	void push_front(const T &data) {
-	    node *new_head = new node;
-	    new_head->data = data;
-	    new_head->next = head;
+	void push_front(T && data) {
+	    // node *new_head = new node;
+	    auto new_head = a.allocate(1);
+
+	    // new_head->data = data;
+	    // new_head->next = head;
+	    a.construct(new_head, std::move(data), head);
 	    head = new_head;
 	}
 
 	~list() {
 	    for (node *p = head; p != nullptr; ) {
 		p = head->next;
-		delete head;
+		// delete head;
+		a.destroy(head);
+		a.deallocate(head, 1);
 		head  = p;
 	    }
 	}
