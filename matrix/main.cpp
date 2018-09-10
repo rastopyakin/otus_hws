3d82b813058ec1aad40b88fdd2ce45542b920b82
Alexander Popov <rastopyakin@gmail.com>
2018-09-11 06:51:45 +0700

    Implement general multidimensional MatrixNdim (matrix)
    Currently only assignment is working.
    Issue: .insert() method of map should be replaced with modifying analog.

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -8,16 +8,29 @@
 
 int main(int argc, char *argv[]) {
 
+
+    Cube<int, 11> cube;
+    std::cout << cube.size() << std::endl;
+
+    std::cout << cube[2][3][4] << std::endl;
+    cube[2][3][4] = 77;
+    std::cout << cube[2][3][4] << std::endl;
+
+    std::cout << cube[1][0][0] << std::endl;
+    cube[1][0][0] = 44;
+    std::cout << cube[1][0][0] << std::endl;
+
+    std::cout << "----------\n";
     Matrix<int, 3> matrix;
-    assert(matrix.size() == 0);
     std::cout << matrix.size() << std::endl;
 
-    Cell<int, 3> cell {nullptr};
-    // cell = 10;
-    int m;
-    m = cell;
-    std::cout << "m = " << m << std::endl;
+    std::cout << matrix[2][4] << std::endl;
+    matrix[2][4] = 100500;
+    std::cout << matrix[2][4] << std::endl;
 
+    matrix[11][17] = 23;
+
+    std::cout << "----------\n";
     Vector<int, 4> vec;
     std::cout << "vec[4] = " <<  vec[4] << std::endl;
 
@@ -27,6 +40,7 @@ int main(int argc, char *argv[]) {
     std::cout << "vec[7000] = " <<  vec[7000] << std::endl;
     vec[537] = 7777;
     vec[17] = 34;
+    vec[17] = 50;
     std::cout << "vec[17] = " << vec[17] << std::endl;
     std::cout << "vec[11] = " << vec[11] << std::endl;
     vec[19] = -3;
--- matrix/matrix.hpp
+++ matrix/matrix.hpp
@@ -11,15 +11,30 @@ using ind_t = std::size_t;
 template<class T, T def_val, ind_t n_dim>
 class MatrixNdim {
 public:
+    using child_type = MatrixNdim<T, def_val, n_dim-1>;
+    using parent_type = MatrixNdim<T, def_val, n_dim+1>;
+
+    MatrixNdim(parent_type * parent=nullptr) : m_parent(parent) {}
     std::size_t size() const {
         return 0;
     }
-    auto operator[] (ind_t ind) {
-        return 0;
+    child_type operator[] (ind_t ind) {
+        auto result = childs.find(ind);
+        if (result != childs.end())
+            return result->second;
+        index_candidate = ind;
+        return child_type {this};
     }
-
+    void store(child_type * child) {
+        childs.insert(std::make_pair(index_candidate, *child));
+        if (m_parent != nullptr)
+            m_parent->store(this);
+        std::cout << index_candidate << ",";
+    }
 private:
-    std::vector<MatrixNdim<T, def_val, n_dim-1>> m;
+    std::map<ind_t, child_type> childs;
+    ind_t index_candidate;
+    parent_type * m_parent;
 };
 
 template<class T, T def_val>
@@ -46,7 +61,9 @@ private:
 template<class T, T def_val>
 class MatrixNdim<T, def_val, 1> {
 public:
+    using parent_type = MatrixNdim<T, def_val, 2>;
     using cell_type = Cell<T, def_val>;
+    MatrixNdim(parent_type *parent=nullptr) : m_parent(parent) {}
     cell_type operator[] (ind_t ind) {
         auto result = cells.find(ind);
         if (result != cells.end())
@@ -59,7 +76,9 @@ public:
     }
     void store(cell_type * cell) {
         cells.insert(std::make_pair(index_candidate, *cell));
-        std::cout << "vec[" << index_candidate << "] = " << *cell << " is stored\n";
+        if (m_parent != nullptr)
+            m_parent->store(this);
+        std::cout << index_candidate << ": " << *cell << " is stored\n";
     }
     auto begin() {
         return cells.begin();
@@ -70,6 +89,15 @@ public:
 private:
     std::map<ind_t, cell_type> cells;
     ind_t index_candidate;
+    parent_type * m_parent;
+};
+
+// inifinite instantiation ceiling
+template<class T, T def_val>
+class MatrixNdim<T, def_val, 5> {
+public:
+    using child_type = MatrixNdim<T, def_val, 4>;
+    void store(child_type *) {}
 };
 
 template<class T, T def_val>
@@ -78,5 +106,8 @@ using Vector = MatrixNdim<T, def_val, 1>;
 
 template<class T, T def_val>
 using Matrix = MatrixNdim<T, def_val, 2>;
+
+template<class T, T def_val>
+using Cube = MatrixNdim<T, def_val, 3>;
 
 #endif /* MATRIX_HPP */
