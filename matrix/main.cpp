d4b2abb45c13abf21c0762047269ddc60a0b8979
Alexander Popov <rastopyakin@gmail.com>
2018-09-12 02:12:09 +0700

    Print filled matrix (matrix)

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tuple>
 
 #include "matrix.hpp"
 
@@ -12,6 +13,19 @@ int main(int argc, char *argv[]) {
         matrix[i][N-i] = N - i;
     }
 
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j < N; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
     std::cout << matrix.size() << std::endl;
+
+    int i, j, val;
+    for (auto m : matrix) {
+        std::tie(i, j, val) = m;
+        std::printf("%d %d %d\n", i, j, val);
+    }
     return 0;
 }
