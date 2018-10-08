7ba925e57b5bb4d307c48acf0ef0b290998e9a52
Alexander Popov <rastopyakin@gmail.com>
2018-10-08 16:21:18 +0700

    Add ToolBar view (editor)

--- editor/consoleView.hpp
+++ editor/consoleView.hpp
@@ -1,6 +1,10 @@
 #ifndef CONSOLEVIEW_HPP
 #define CONSOLEVIEW_HPP
 
+#include <iostream>
+#include <string>
+#include <functional>
+
 #include "view.hpp"
 #include "core.hpp"
 
@@ -26,11 +30,55 @@ private:
     EditorCore* core;
 };
 
+class ActiveView {
+public:
+    using isActiveCallBack_t = std::function<bool()>;
+    using callback_t = std::function<void()>;
+    ActiveView(std::string name, isActiveCallBack_t f = []() {return true;})
+        : m_name(name), isActive(f) {}
+    void render() const {
+        if (isActive())
+            std::cout << m_name;
+    }
+    void clicked() const {
+        for (const auto& slot : slots)
+            slot();
+    }
+private:
+    std::string m_name;
+    isActiveCallBack_t isActive;
+    std::vector<callback_t> slots;
+};
+
+class ToolBar : public View {
+public:
+    ToolBar(EditorCore *core) {
+        auto isFileOpened = [core]() {return core->getFileName().size();};
+        buttons.emplace_back("newFile");
+        buttons.emplace_back("openFile");
+        buttons.emplace_back("saveFile", isFileOpened);
+        buttons.emplace_back("addFigure", isFileOpened);
+        buttons.emplace_back("removeFigure", isFileOpened);
+    }
+    void render() const override {
+        std::cout << "Toolbar: ";
+        for (const auto& b : buttons) {
+            b.render();
+            std::cout << ' ';
+        }
+        std::cout << std::endl;
+    }
+private:
+    // EditorCore* core;
+    std::vector<ActiveView> buttons;
+};
+
 class ConsoleView : public View {
 public:
     ConsoleView(EditorCore* p) {
-        views.push_back(std::make_unique<WorkSheet>(p));
         views.push_back(std::make_unique<FileNameView>(p));
+        views.push_back(std::make_unique<ToolBar>(p));
+        views.push_back(std::make_unique<WorkSheet>(p));
     }
     void render() const override {
         for (const auto& v : views)
--- editor/main.cpp
+++ editor/main.cpp
@@ -1,5 +1,5 @@
 #include "core.hpp"
-#include "controller.hpp"
+// #include "controller.hpp"
 #include "consoleView.hpp"
 
 void menuNewDoc() {}
@@ -17,11 +17,15 @@ int main(int argc, char *argv[]) {
     ConsoleView view {&core};
     core.setView(&view);
 
+    view.render();
+
     core.newDoc("file_1");
     core.addFig(std::make_unique<Triangle>(0));
     core.addFig(std::make_unique<Circle>(1));
 
     core.removeFig(0);
+
+    core.saveDoc();
 
     core.newDoc("file_2");
     return 0;
