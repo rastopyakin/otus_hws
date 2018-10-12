86d5fb747bcf2d3ae31f0df2d853ddcfd44bca17
Alexander Popov <rastopyakin@gmail.com>
2018-10-13 05:02:02 +0700

    Implement dynamic blocks feature (bulk)

--- bulk/collector.hpp
+++ bulk/collector.hpp
@@ -13,12 +13,27 @@ public:
     void gather(const command_t &cmd) {
         if (getCurrentBlockSize() == 0)
             time_stamp = std::chrono::steady_clock::now();
-        cmd_list.push_back(cmd);
-        if (getCurrentBlockSize() == block_size) {
+
+        if (cmd == "{") {
+            if (dynamic_size_block_depth==0)
+                flush();        // forced block interruption before dynamic block
+            dynamic_size_block_depth++;
+        }
+        else if (cmd == "}") {
+            dynamic_size_block_depth--;
+            if (dynamic_size_block_depth==0)
+                flush();        // dynamic block is ready here
+        }
+        else
+            cmd_list.push_back(cmd);
+
+        // submit fixed size block if it's ready
+        if (getCurrentBlockSize()==block_size and dynamic_size_block_depth==0) {
             flush();
         }
     }
-    std::size_t getCurrentBlockSize() {
+
+    std::size_t getCurrentBlockSize() const {
         return cmd_list.size();
     }
 
@@ -31,10 +46,13 @@ public:
     }
 
     void flush() {
-        notify_all();
+        if (dynamic_size_block_depth == 0 and getCurrentBlockSize() != 0)
+            notify_all();
         cmd_list.clear();
+        dynamic_size_block_depth = 0;
     }
 private:
+    int dynamic_size_block_depth = 0;
     std::chrono::time_point<std::chrono::steady_clock> time_stamp;
     std::size_t block_size;
     std::list<command_t> cmd_list;
