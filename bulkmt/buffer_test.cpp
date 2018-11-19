241d53582c07f9e4c7e0a289f7535b2a516efaf3
Alexander Popov <rastopyakin@gmail.com>
2018-11-20 00:05:10 +0700

    Extract Buffer class from Logger, they decomposed (bulkmt)

--- bulkmt/buffer_test.cpp
+++ bulkmt/buffer_test.cpp
@@ -8,25 +8,53 @@
 #include <future>
 
 template<class T>
+class Buffer {
+public:
+    template<class U>
+    void push(U &&elem) {
+        std::lock_guard<std::mutex> lk{buffer_lock};
+        buffer.push(std::forward<U>(elem));
+        push_cond.notify_one();
+    }
+
+    std::shared_ptr<T> pop_or_wait() {
+        std::unique_lock<std::mutex> lk{buffer_lock};
+        push_cond.wait(lk, [this] {return !buffer.empty() or stop_flag.load();});
+        if (buffer.empty()) {
+            return std::shared_ptr<T>{};
+        }
+        auto elem = std::make_shared<T>(std::move(buffer.front()));
+        buffer.pop();
+        return elem;
+    }
+    void disable_waiting() {
+        stop_flag.store(true);
+        std::lock_guard<std::mutex> lk{buffer_lock};
+        push_cond.notify_all();
+    }
+private:
+    std::queue<T> buffer;
+    std::mutex buffer_lock;
+    std::condition_variable push_cond;
+    std::atomic_bool stop_flag = false;
+};
+
+template<class T>
 class Logger {
 public:
     Logger() {
         worker = std::thread{[this] {process();}};
     }
-    template<class U>
-    void push(U &&elem) {
-        std::lock_guard<std::mutex> lk{buffer_mutex};
-        buffer.push(std::forward<U>(elem));
-        push_cv.notify_one();
-    }
     auto getStat() {
         return statistics.get_future();
     }
+    void notify(const std::string &str) {
+        buffer.push(str);
+    }
     ~Logger() {
-        stop_flag.store(true);
-        while (!is_finished_flag.load())
-            push_cv.notify_all();
-
+        buffer.disable_waiting();
+        // while (!is_finished_flag.load());
+            // push_cv.notify_all();
         std::cerr << "wait join\n";
         worker.join();
     }
@@ -35,35 +63,20 @@ private:
         int lines_counter = 0;
         std::cerr << "processing\n";
         while (true) {
-            std::unique_lock<std::mutex> lk{buffer_mutex};
-            push_cv.wait(lk, [this] {return !buffer.empty() or stop_flag.load();});
-            if (buffer.empty()) {
-                is_finished_flag.store(true);
+            auto elem = buffer.pop_or_wait();
+            if (!elem)
                 break;
-            }
-            auto elem = buffer.front();
-            buffer.pop();
-            lk.unlock();
 
             lines_counter++;
-            std::cout << elem << std::endl;
+            std::cout << *elem << std::endl;
         }
 
         statistics.set_value(lines_counter);
         std::cerr << "value set\n";
-        // while (!buffer.empty()) {
-        //     std::cout << "rest\n";
-        //     std::cout << buffer.front() << std::endl;
-        //     buffer.pop();
-        // }
     }
+    Buffer<std::string> buffer;
     std::promise<int> statistics;
-    std::queue<T> buffer;
-    std::mutex buffer_mutex;
     std::thread worker;
-    std::condition_variable push_cv;
-    std::atomic_bool stop_flag = false;
-    std::atomic_bool is_finished_flag = false;
 };
 
 int main(int argc, char *argv[]) {
@@ -73,11 +86,10 @@ int main(int argc, char *argv[]) {
         stat = logger.getStat();
         std::string str;
         while (std::getline(std::cin, str)) {
-            logger.push(str);
+            logger.notify(str);
         }
     }
 
-    std::cerr << "input taken\n";
     std::cerr << stat.get() << " lines\n";
     return 0;
 }
